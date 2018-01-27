46f057f56de7ee17bfdffc20e416efcec491eb7f
Rorschach Rev <rorschachrev@gmail.com>
2018-01-26 21:22:12 -0700

    Maybe Fixed after Overwrite

--- mediacoin/settings.py
+++ mediacoin/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+<<<<<<< Updated upstream
 #from settings_secret import *
+=======
+from settings_secret import *
+>>>>>>> Stashed changes
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
